821fc824974d6bffbf7f3592144cfd8ee0b74b2b
Alex Grape <alex@beholder.ai>
2019-03-03 21:21:34 +0200

    paypal verify

--- shop/views.py
+++ shop/views.py
@@ -266,40 +266,32 @@ def thankyou(request):
             request.session.modified = True
     return render(request, 'shop/thankyou.html')
 
+
 @csrf_exempt
 def paypal(request):
     VERIFY_URL_PROD = 'https://ipnpb.paypal.com/cgi-bin/webscr'
     VERIFY_URL_TEST = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr'
-
     # Switch as appropriate
     VERIFY_URL = VERIFY_URL_TEST
-
     # CGI preamble
-    print('content-type: text/plain')
     print(request.POST.dict())
+    answer = request.POST.dict()
+    dictlist = list(answer.items())
+    dictlist.append(('cmd', '_notify-validate'))
+    # Post back to PayPal for validation
+    headers = {'content-type': 'application/x-www-form-urlencoded',
+               'user-agent': 'Python-IPN-Verification-Script'}
+    r = requests.post(VERIFY_URL, params=dictlist, headers=headers, verify=True)
+    r.raise_for_status()
 
-    # # Read and parse query string
-    # param_str = sys.stdin.readline().strip()
-    # params = urllib.parse.parse_qsl(param_str)
-    #
-    # # Add '_notify-validate' parameter
-    # params.append(('cmd', '_notify-validate'))
-    #
-    # # Post back to PayPal for validation
-    #
-    # headers = {'content-type': 'application/x-www-form-urlencoded',
-    #            'user-agent': 'Python-IPN-Verification-Script'}
-    # r = requests.post(VERIFY_URL, params=params, headers=headers, verify=True)
-    # r.raise_for_status()
-    #
-    # # Check return message and take action as needed
-    # if r.text == 'VERIFIED':
-    #     print('VERIFIED')
-    #     pass
-    # elif r.text == 'INVALID':
-    #     print('INVALID')
-    #     pass
-    # else:
-    #     pass
+    # Check return message and take action as needed
+    if r.text == 'VERIFIED':
+        print('VERIFIED')
+        pass
+    elif r.text == 'INVALID':
+        print('INVALID')
+        pass
+    else:
+        pass
 
     return JsonResponse("Thanks!", safe=False)
