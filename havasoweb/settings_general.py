05e31b2893ad3d366da699a57641ef870211031a
Alex Grape <alex@beholder.ai>
2019-06-05 23:43:25 +0300

    fixed collect static bug

--- havasoweb/settings_general.py
+++ havasoweb/settings_general.py
@@ -120,7 +120,12 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static'),
 # )
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "home/static"),
+]
+
+
 
 STATIC_URL = '/static/'
 
