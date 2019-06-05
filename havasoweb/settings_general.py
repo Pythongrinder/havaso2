34ed931dfdbe15b1f64f76e6f4f81079976c5e63
Alex Grape <alex@beholder.ai>
2019-06-05 17:36:16 +0300

    added email to 503.html page

--- havasoweb/settings_general.py
+++ havasoweb/settings_general.py
@@ -120,8 +120,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = 'media'
